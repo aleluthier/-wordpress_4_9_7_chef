d8cffd28eafca79effef93bba112bf074bbd8e76
esdrasrios <33075705+esdrasrios@users.noreply.github.com>
2017-11-13 13:12:55 -0200

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -18,8 +18,8 @@
  * @package WordPress
  */
 
-define('WP_HOME','http://ELBwp-1706345073.eu-west-2.elb.amazonaws.com');
-define('WP_SITEURL','http://ELBwp-1706345073.eu-west-2.elb.amazonaws.com');
+define('WP_HOME','http://d1c1wyjk93pyo1.cloudfront.net');
+define('WP_SITEURL','http://d1c1wyjk93pyo1.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
